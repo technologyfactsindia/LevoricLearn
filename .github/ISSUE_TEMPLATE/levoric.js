8a4f75ed3cf0e9a8b6d4831d2fc131e623dc1375
technologyfactsindia <140900567+technologyfactsindia@users.noreply.github.com>
2023-07-29 19:45:48 +0530

    Add files via upload

---
+++ .github/ISSUE_TEMPLATE/levoric.js
@@ -0,0 +1,21 @@
+function search_programming() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("Levoric-course-list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+// DarkMode Of Levoric //
+function levoric_Programming() {
+    var levoriclearn = document.body;
+    levoriclearn.classList.Dark("Dark-mode");
+}
